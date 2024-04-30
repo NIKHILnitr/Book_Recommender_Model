aed9e20792ff480eb5bc3a5295460f03870b6441
Nikhil Bhoi <140872474+NIKHILnitr@users.noreply.github.com>
2024-04-30 23:54:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
                            book_name = list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
+                           votes=list(popular_df['num_ratings'].values),  
                            rating=list(popular_df['avg_rating'].values)
                            )
 
